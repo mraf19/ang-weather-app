4e98568d5f29a3e0906b3c95b194922a6626aba8
mraf19 <raflimr1902@gmail.com>
2023-08-24 11:32:16 +0700

    format code

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
+import { FormControl } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -9,7 +10,7 @@ import { DegreePipe } from './degree.pipe';
 @NgModule({
   declarations: [AppComponent, DegreePipe],
   imports: [BrowserModule, AppRoutingModule, HttpClientModule],
-  providers: [],
+  providers: [DegreePipe],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
